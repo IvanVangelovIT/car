de5c443fa27d8336ced847a2c437c99bcdd7bf97
IRV <ivanrvangelovit@gmail.com>
2025-07-29 18:31:29 +0300

    Init

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Добавихме този маршрут:
+app.get('/', (req, res) => {
+  res.send('Здравей! Това е началната страница на backend-а!');
+});
+
+// Предишният маршрут
+app.get('/api/health', (req, res) => {
+  res.json({ status: 'Backend is working!' });
+});
+
+app.listen(5000, () => {
+  console.log('Server running on http://localhost:5000');
+});
