3ba240f77b34ed224c2186760d18ab192ead55dc
IRV <ivanrvangelovit@gmail.com>
2025-08-06 19:51:02 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'Backend is working!' });
 });
 
-// Важно: използваме PORT от средата
+// Важно: използваме PORT отa средата
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
