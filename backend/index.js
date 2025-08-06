e3868223fec3e79f2175951c6e166a919cd7e989
IRV <ivanrvangelovit@gmail.com>
2025-08-06 20:37:36 +0300

    .

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Начален маршрут
-app.get(`${process.env.REACT_APP_API_URL}`, (req, res) => {
+// Това е правилният начин
+app.get('/', (req, res) => {
   res.send('Здравей! Това е началната страница на backend-а!');
 });
 
-// Health check маршрутa
-app.get(`${process.env.REACT_APP_API_URL}/api/health`, (req, res) => {
+app.get('/api/health', (req, res) => {
   res.json({ status: 'Backend is working!' });
 });
 
-// Важно: използваме PORT отa средата
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
